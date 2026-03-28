9ae4a31afc55f0c9eb628cf7ba47ef3539c4db0f
denayee <nayeedevarshi@gmail.com>
2026-03-28 11:57:25 +0530

    remove tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { Metadata, Viewport } from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import '../styles/tailwind.css';
 import ScrollToTop from '../components/ScrollToTop';
 
@@ -32,9 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           <ScrollToTop />
           {children}
-
-          <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fdevarshipo9436back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-          <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>

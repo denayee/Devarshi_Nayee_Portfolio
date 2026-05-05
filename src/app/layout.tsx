2929fa6917299bc3518c06e23cb9621a18d732d1
denayee <nayeedevarshi@gmail.com>
2026-05-05 08:04:36 +0530

    versal analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Metadata, Viewport } from 'next';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import '../styles/tailwind.css';
 import ScrollToTop from '../components/ScrollToTop';
 
@@ -34,6 +35,7 @@ export default function RootLayout({
           <ScrollToTop />
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
